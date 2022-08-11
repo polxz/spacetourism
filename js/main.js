3b929576a747202537db52849a573fba7f1aa9a1
polxz <80154400+polxz@users.noreply.github.com>
2022-08-11 14:26:34 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrow__left" src="images/arrow 2.svg" alt="">',
+        nextArrow: '<img class="slider__arrow slider__arrow__right" src="images/arrow 1.svg" alt="">',
+    });
+});
